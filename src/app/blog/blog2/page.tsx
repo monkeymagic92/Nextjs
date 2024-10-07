6a26040cf2b963fafb9bd3d9dc72f3ebe079d59b
monkeymagic92 <ddw0099@naver.com>
2024-10-07 17:30:33 +0900

    test

---
+++ src/app/blog/blog2/page.tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+    return (
+        <div>
+            <h1>Welcome to the Blog Page222222222</h1>
+            <p>This is the content of the blog page.</p>
+        </div>
+    );
+}
