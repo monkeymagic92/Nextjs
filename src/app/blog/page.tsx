f47234caf078846fe2c2ce15cda323f713c701dd
이재용 <ljy@ieetu.com>
2025-02-07 15:17:38 +0900

    테스트용 코드 추가 ( router.push )

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,29 +1,37 @@
 'use client'; // 이 지시어를 추가하여 클라이언트 컴포넌트로 만듭니다.
 
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Blog() {
+
+    const router = useRouter();
 
     // 여러 값을 담기 위해 객체로 상태 관리
     const [result, setResult] = useState<{ message?: string; testMessage?: string } | null>(null);
 
+    // SML 서버에서 api 호출
     const fetchData = async () => {
         try {
-            const response = await fetch('/api/v1/board/list'); // GET 요청으로 API 호출
+            const response = await fetch('/api/v1/board/list');  // rewrites를 활용해 프록시 처리
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const result = await response.json();
-            console.log(result); // API 응답 콘솔에 출력
+            console.log(result);  // JSON 응답 출력
 
-            // 여러 값들을 상태에 저장
             setResult({
                 message: result.message,
                 testMessage: result.testMessage,
             });
         } catch (error) {
-            console.error('Error fetching data:', error); // 에러 발생 시 콘솔에 출력
+            console.error('Error fetching data:', error);
         }
+    };
+
+    // 메인으로 이동
+    const fnMoveToMain = () => {
+        router.push('/');
     };
 
     return (
@@ -42,6 +50,8 @@ export default function Blog() {
             )}
 
             <h1></h1>
+
+            <button className='btn btn-primary' onClick={fnMoveToMain}>메인으로 이동</button>
         </div>
     );
 }
--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -2,11 +2,21 @@
 
 import React from 'react';
 import styles from './Header.module.css';  // CSS 모듈 불러오기
+import { useRouter } from 'next/navigation';
 
 const Header: React.FC = () => {
+
+    const router = useRouter();
+    // 메인으로 이동
+    const fnMoveToMain = () => {
+        router.push('/blog');
+    };
+
+
     return (
         <header className={styles.header}>
             <h1>My Website Header★★★★★</h1>
+            <button className='btn btn-primary' onClick={fnMoveToMain}>메인으로 이동</button>
         </header>
     );
 };
